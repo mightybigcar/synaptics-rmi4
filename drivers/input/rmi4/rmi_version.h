7eac479ba909ef3277c937bdd782f55468211690
Christopher Heiny <cheiny@synaptics.com>
2013-05-31 12:48:33 -0700

    Bumped subminor version.

--- drivers/input/rmi4/rmi_version.h
+++ drivers/input/rmi4/rmi_version.h
@@ -3,11 +3,11 @@
 
 #define RMI_VERSION_MAJOR 1
 #define RMI_VERSION_MINOR 8
-#define RMI_VERSION_SUBMINOR 0
+#define RMI_VERSION_SUBMINOR 1
 
-#define RMI_VERSION_BRANCH github
-#define RMI_EXTRA_NUMBER 2
-#define RMI_EXTRA_STRING github.0
+#define RMI_VERSION_BRANCH master
+#define RMI_EXTRA_NUMBER 0
+#define RMI_EXTRA_STRING master.0
 
 #define rmi_tostr(s) #s
 #define rmi_str(s) rmi_tostr(s)
