d86eea514ad42fdcff48533a1b0ef6f81710e800
Christopher Heiny <cheiny@synaptics.com>
2013-11-11 14:52:36 -0800

    Fix overlong lines, convert mdelay() to msleep(), convert sprintf() to snprintf().

--- drivers/input/rmi4/rmi_i2c.c
+++ drivers/input/rmi4/rmi_i2c.c
@@ -147,7 +147,7 @@ static int copy_to_debug_buf(struct device *dev, struct rmi_i2c_data *data,
 	temp = data->debug_buf;
 
 	for (i = 0; i < len; i++) {
-		n = sprintf(temp, " %02x", buf[i]);
+		n = snprintf(temp, 3, " %02x", buf[i]);
 		temp += n;
 	}
 
@@ -282,12 +282,13 @@ static int rmi_i2c_probe(struct i2c_client *client,
 				retval);
 			return retval;
 		}
-		mdelay(pdata->reset_delay_ms ? pdata->reset_delay_ms : DEFAULT_RESET_DELAY_MS);
+		msleep(pdata->reset_delay_ms ?
+				pdata->reset_delay_ms : DEFAULT_RESET_DELAY_MS);
 		dev_info(&client->dev, "Done with GPIO configuration.\n");
 	}
 
-	rmi_transport = devm_kzalloc(&client->dev, sizeof(struct rmi_transport_device),
-				GFP_KERNEL);
+	rmi_transport = devm_kzalloc(&client->dev,
+			sizeof(struct rmi_transport_device), GFP_KERNEL);
 
 	if (!rmi_transport)
 		return -ENOMEM;
